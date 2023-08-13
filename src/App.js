66ad6333fb34204eb0b25ff25a844d2a400f0f3d
vincent <yunchenc@umich.edu>
2023-08-13 20:35:29 +0800

    basic html css structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>$400<span>.00</span></h1>
+      <form>
+        <div className='basic'>
+          <input type="text" placeholder='20 lunch with David' />
+          <input type="datetime-local"/>
+        </div>
+        <div className='description'>
+          <input type="text" placeholder='description'/>
+        </div>
+        <button type="submit">Add new transaction</button>
+      </form>
+
+      <div className="transactions">
+        <div className="transaction">
+          <div className="left">
+            <div className='name'>lunch with David</div>
+            <div className='description'>lobster lunch</div>
+          </div>
+
+          <div className="right">
+            <div className='price'>$20</div>
+            <div className='datetime'>2023-6-3 15:55</div>
+          </div>
+        </div>
+
+        <div className="transaction">
+          <div className="left">
+            <div className='name'>lunch with David</div>
+            <div className='description'>lobster lunch</div>
+          </div>
+
+          <div className="right">
+            <div className='price'>$20</div>
+            <div className='datetime'>2023-6-3 15:55</div>
+          </div>
+        </div>
+
+      </div>
+    </main>
   );
 }
 
