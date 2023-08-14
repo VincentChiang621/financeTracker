6848b4a8c4d0afd905bf724e116e6c2c48af01f6
vincent <yunchenc@umich.edu>
2023-08-14 14:28:41 +0800

    api, backend done

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config()
+const Transaction = require('./models/Transaction')
+const mongoose = require('mongoose')
+const app = express()
+
+
+app.use(cors());
+app.use(express.json())
+app.get('/api/test', (req, res) => {
+    res.json({ message: 'test ok' });
+});
+
+app.post('/api/transaction', async(req, res) => {
+    await mongoose.connect(process.env.MONGO_URL);
+    const{name,description,date,price} = req.body;
+    const transaction = await Transaction.create({name, description, date, price});
+    res.json(transaction);
+})
+
+const PORT = process.env.PORT || 4040; 
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
+
+
+
+
--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+  const[name, setName] = useState('');
+  const[date, setDate] = useState('');
+  const[description, setDescription]= useState('');
+
+  function addNewTransaction(ev) {
+    ev.preventDefault();
+    const url = process.env.REACT_APP_API_URL+'/transaction';
+    const price = name.split(' ')[0];
+    fetch(url, {
+      method: 'POST',
+      headers: {'Content-type': 'application/json'},
+      body: JSON.stringify({
+        price,
+        name:name.substring(price.length+1), 
+        description, 
+        date,
+
+      })
+    }).then(response => {
+      response.json().then(json => {
+        setName('');
+        setDate('');
+        setDescription('');
+        console.log('result', json)
+      })
+    })
+  }
+
   return (
     <main>
       <h1>$400<span>.00</span></h1>
-      <form>
+      <form onSubmit={addNewTransaction}>
         <div className='basic'>
-          <input type="text" placeholder='20 lunch with David' />
-          <input type="datetime-local"/>
+          <input 
+            type="text" 
+            placeholder='20 lunch with David' 
+            value={name}
+            onChange={ev => setName(ev.target.value)}/>
+          <input 
+            type="datetime-local"
+            value={date}
+            onChange={ev => setDate(ev.target.value)}/>
         </div>
         <div className='description'>
-          <input type="text" placeholder='description'/>
+          <input 
+            type="text" 
+            value={description}
+            onChange={ev => setDescription(ev.target.value)}
+            placeholder='description'/>
         </div>
         <button type="submit">Add new transaction</button>
       </form>
